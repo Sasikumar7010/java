43b25d296d533af498ffee2ddc986ba3bae04935
Sasikumar7010 <mbro25458@gmail.com>
2025-03-06 15:08:47 +0530

    Add files via upload

---
+++ n.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class n {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);//object
+
+        String day ;
+        System.out.println(" enter a day:");
+        day = sc.nextLine();
+
+        switch(day){
+            case "monday" -> System.out.println("The worst day 😫😫😫😫😫"); // enhanced switch
+
+            case "tuesday" -> System.out.println("The day is boooring 😪😪😪😪😪");
+
+            case "wednesday" -> System.out.println("The day is ok 😮😮😮😮😮");
+
+            case "thursday" -> System.out.println("The day good 🙄🙄🙄🙄🙄🙄");
+
+            case "friday" -> System.out.println("The day is feel better 🙂🙂🙂🙂");
+
+            case "saturday" -> System.out.println("Today i feel the weakend vibe 😚😚😚😚😚😚");
+
+            case "sunday" -> System.out.println("The enjoyable day 😎😎😎😎😎😎😎😎😎🥰🥰🥰👀👀👀👀");
+
+            default -> System.out.println("Enter the valid day !!!!!!!");
+
+        }
+    }
+}
